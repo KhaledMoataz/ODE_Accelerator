740400fe7f976506716f0bba8d45277fb5861eac
AhmedEssamDakrory <ahmed.edakrory@gmail.com>
2020-03-21 22:58:04 +0200

    no shift

--- Fixed Point Arithmetic/src/fixed_point_mult.v
+++ Fixed Point Arithmetic/src/fixed_point_mult.v
@@ -16,6 +16,6 @@ wire signed [31:0] round;
 assign round = 32'b00000000000000000000000001000000; 
 assign out = (multiplicand*multiplier)+round;
 assign overflow_flag = !(( out[31:22] == 10'b0000000000 ) | ( out[31:22] == 10'b1111111111 )); 
-assign result = out>>>7;
+assign result = out[22:7];
 
 endmodule
