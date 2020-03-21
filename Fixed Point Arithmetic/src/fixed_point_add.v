37a46279079d935aea38cf924184390546966cbf
Ayman <ayman.elakwah@gmail.com>
2020-03-21 03:11:20 +0200

    testbench for fixed point adder & multiplier

--- Fixed Point Arithmetic/src/fixed_point_add.v
+++ Fixed Point Arithmetic/src/fixed_point_add.v
@@ -3,7 +3,7 @@ module adder(A, B, result, overflow_flag);
 input signed [15:0] A;
 input signed [15:0] B;
 output signed [15:0] result;
-input overflow_flag;
+output overflow_flag;
 
 assign result = A+B;
 assign overflow_flag = !((A[15]&B[15]&result[15])|(!A[15]&!B[15]&!result[15]));
---
+++ Fixed Point Arithmetic/src/fixed_point_add_testbench.v
@@ -0,0 +1,24 @@
+module fixed_point_add_testbench;
+    reg [15:0] A;
+    reg [15:0] B;
+    wire [15:0] result;
+    reg [15:0] expected_result;
+    wire overflow_flag;
+    reg expected_overflow_flag;
+    reg[48:0] read_data [0:2];
+
+    adder adder_unit(A, B, result, overflow_flag);
+    integer i;
+
+    initial
+    begin
+        $readmemb("adder_test_cases.txt", read_data);
+        for (i=0; i<2; i=i+1)
+        begin
+            {A, B, expected_result, expected_overflow_flag} = read_data[i];
+            #20
+            if (result != expected_result || overflow_flag != expected_overflow_flag)
+                $display("%d : FAILED", i);
+        end
+    end
+endmodule
