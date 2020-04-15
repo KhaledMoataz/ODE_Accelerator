fe6883f5c6ad6c8bfb96e556511541287739392d
Ayman <ayman.elakwah@gmail.com>
2020-04-15 09:19:22 +0200

    make fixed point adder generic

--- Fixed Point Arithmetic/src/fixed_point_add.v
+++ Fixed Point Arithmetic/src/fixed_point_add.v
@@ -1,11 +1,14 @@
-module adder(A, B, result, overflow_flag);
+module adder
+       #(parameter N = 16)
+       (A, B, result, overflow_flag, carry);
 
-input signed [15:0] A;
-input signed [15:0] B;
-output signed [15:0] result;
+input signed [N - 1:0] A;
+input signed [N - 1:0] B;
+output signed [N - 1:0] result;
 output overflow_flag;
+output carry;
 
-assign result = A+B;
-assign overflow_flag = ((A[15]&B[15]&!result[15])|(!A[15]&!B[15]&result[15]));
+assign {carry, result} = {1'b0, A} + {1'b0, B};
+assign overflow_flag = ((A[N - 1]&B[N - 1]&!result[N - 1])|(!A[N - 1]&!B[N - 1]&result[N - 1]));
 
 endmodule
