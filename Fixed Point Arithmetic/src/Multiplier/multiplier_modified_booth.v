aacea16f8be3d70e5b75617c24404f9b5e20b451
Ayman <ayman.elakwah@gmail.com>
2020-04-17 00:47:13 +0200

    add carry select adder

--- Fixed Point Arithmetic/src/Multiplier/multiplier_modified_booth.v
+++ Fixed Point Arithmetic/src/Multiplier/multiplier_modified_booth.v
@@ -20,7 +20,7 @@ module multiplier_modified_booth(
 
     // 2's Complement of multiplicand
     wire [15:0] multiplicand_complement;
-    adder adder_multiplicand_complement(.A(~multiplicand_buffer), .B(16'h01), .result(multiplicand_complement), .overflow_flag(), .carry());
+    carry_select_adder adder_multiplicand_complement(.A(~multiplicand_buffer), .B(16'h01), .cin(1'b0), .result(multiplicand_complement), .overflow_flag(), .carry());
     // Double multiplicand
     wire [16:0] double_multiplicand;
     assign double_multiplicand = {multiplicand_buffer[15:0], 1'b0};
@@ -45,20 +45,20 @@ module multiplier_modified_booth(
                                 {A[15], 16'h0000};
 
     // Main adder used in loop
-    adder adder_loop(.A(A), .B(B[15:0]), .result(AB), .overflow_flag(), .carry());
+    carry_select_adder adder_loop(.A(A), .B(B[15:0]), .cin(1'b0), .result(AB), .overflow_flag(), .carry());
     // Rounding
     wire [31:0] round;
     wire [31:0] result_after_round;
     assign round = 32'h00000040;
-    adder#(32) adder_round(.A({A, Q[16:1]}), .B(round), .result(result_after_round), .overflow_flag(), .carry());
+    carry_select_adder#(32) adder_round(.A({A, Q[16:1]}), .B(round), .cin(1'b0), .result(result_after_round), .overflow_flag(), .carry());
     // Final Result
     assign result = result_after_round[22:7];
     assign overflow_flag = !((result_after_round[31:22] == 10'b0000000000) | (result_after_round[31:22] == 10'b1111111111));
     // Counter
     reg [2:0] counter;
     wire [2:0] counter_new_value;
-    wire couter_carry;
-    adder#(3) adder_counter(.A(counter), .B(3'h1), .result(counter_new_value), .overflow_flag(), .carry(counter_carry));
+    wire counter_finish;
+    carry_select_adder#(4) adder_counter(.A({1'b0, counter}), .B(4'h1), .cin(1'b0), .result({counter_finish, counter_new_value}), .overflow_flag(), .carry());
 
     always @(posedge start) begin
         start_buffer <= 1;
@@ -80,7 +80,7 @@ module multiplier_modified_booth(
             A <= {B[16], B[16], AB[15:2]};
             Q <= {AB[1], AB[0], Q[16:2]};
             counter <= counter_new_value;
-            finish <= counter_carry;
+            finish <= counter_finish;
         end
     end
 endmodule
