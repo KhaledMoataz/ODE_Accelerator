16e7a6f48e8e0b7840145a5333f0221e41343952
Omar <IamAdeadfish@hotmail.com>
2020-04-17 18:27:45 +0200

    [Modified] fsm end with 2 output==state

--- Eular Module/DONE/FSM_END_EULAR.v
+++ Eular Module/DONE/FSM_END_EULAR.v
@@ -1,6 +1,6 @@
 module FSM_END_EULAR (
 input clk,rst_sync,rst_async,F,R,D,
-output outp
+output [1:0] outp
 );
 
 // States
@@ -8,14 +8,12 @@ output outp
 localparam  State0 = 2'b00, State1 = 2'b10 , State2 = 2'b11;
 reg[1:0] current_state;
 
-reg temp_out;
 
-assign outp = temp_out;
+assign outp = current_state;
 
 initial 
     begin
         current_state <= State0;
-        temp_out <= 1'b0;
     end
 
 always @(posedge clk or posedge rst_async)
@@ -23,7 +21,6 @@ begin
     if(rst_sync == 1'b1 || rst_async == 1'b1)
         begin
             current_state <= State0;
-            temp_out <= 1'b0;
         end
     else
     case(current_state)
@@ -32,7 +29,6 @@ begin
                 if(F == 1'b1)
                     begin
                         current_state <= State1;
-                        temp_out <= 1'b0;
                     end
             end
         State1:
@@ -40,7 +36,6 @@ begin
                 if(R == 1'b1)
                     begin
                         current_state <= State2;
-                        temp_out <= 1'b0;
                     end
             end
         State2:
@@ -48,7 +43,6 @@ begin
                 if(D == 1'b1)
                     begin
                         current_state <= State0;
-                        temp_out <= 1'b1;
                     end
             end
     endcase
