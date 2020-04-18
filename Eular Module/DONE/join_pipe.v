de6c99c36d8821b6cf4334c67d00780bae7208e1
Omar <IamAdeadfish@hotmail.com>
2020-04-18 21:57:24 +0200

    [fixed] now writes from 1st place not 2nd

--- Eular Module/DONE/join_pipe.v
+++ Eular Module/DONE/join_pipe.v
@@ -1,4 +1,4 @@
-module LAST_PIPE  #(parameter ADD_SIZE = 16, parameter DATA_SIZE = 16)
+module JOIN_PIPE  #(parameter ADD_SIZE = 16, parameter DATA_SIZE = 16)
 (
  input rst,
  input clk,
@@ -7,19 +7,23 @@ module LAST_PIPE  #(parameter ADD_SIZE = 16, parameter DATA_SIZE = 16)
  input [DATA_SIZE-1:0] data_pipe2,
  input [DATA_SIZE-1:0] h_step,
 //  inout [DATA_SIZE-1:0] result,
- input first_time,
- output carry_out,
- output overflow_flag,
- output negative
+ output overflow_flag
+
 );
 
+
+          /////////// dummy  ////////////
 wire carry_in = 1'b0;
+wire negative;
+wire carry_out;
+        /////////////////////////////////
 wire [DATA_SIZE-1:0] result_add;
 wire [DATA_SIZE-1:0] result_mul;
 wire done_mul;
 
 wire [ADD_SIZE-1:0] pc_answer;
 wire [ADD_SIZE-1:0] inc_pc_in;
+
 
 carry_select_adder add(data_pipe1,data_pipe2,carry_in,result_add,carry_out,overflow_flag,negative);
 
@@ -29,11 +33,10 @@ multiplier_modified_booth multiplier(clk, rst, result_add, h_step, start_mul, re
 incrementor #(ADD_SIZE) inc_pc(inc_pc_in, pc_answer);
 Register #(ADD_SIZE) pc_write(clk, rst, done_mul, pc_answer, inc_pc_in);
 
-ass
 
 // last parameter data2out is not connected (not needed)
 // if not synthesizable add dummy wire
-RAM #(ADD_SIZE,DATA_SIZE) ram(~clk, rst, done_mul, pc_answer, 1'b0, result_mul); 
+RAM #(ADD_SIZE,DATA_SIZE) ram(~clk, rst, done_mul, inc_pc_in, 1'b0, result_mul); 
 
 
 endmodule
