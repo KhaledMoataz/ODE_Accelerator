cc1f48ff2feba15b722a9d0be21a650e9395f362
Yousif Ahmed <YousifGAhmed@gmail.com>
2020-04-19 20:53:40 +0200

    Dummy test is working in the module(no actual RAM)

--- Interpolation Module/Interpolation_Module.v
+++ Interpolation Module/Interpolation_Module.v
@@ -2,10 +2,11 @@ module InterpolationModule #(parameter WORD_SIZE = 16, ADDRESS_WIDTH = 16)
 (
     input start_sg, alert_sg, update_sg, init_sg, clk, rst,
     input [WORD_SIZE-1: 0] tk_port, 
-    inout [WORD_SIZE-1: 0] ram_data1, ram_data2,
+    input [WORD_SIZE-1: 0] ram_data1, ram_data2,
     input [ADDRESS_WIDTH-1 : 0] uk_port,
     output done_sg,mem_write,
-    output [ADDRESS_WIDTH-1 : 0] ram_add1, ram_add2
+    output [ADDRESS_WIDTH-1 : 0] ram_add1, ram_add2,
+    output [WORD_SIZE-1:0] data_to_ram
 
 );
     //enables
@@ -53,9 +54,9 @@ wire un_add_en,uz_add_en,uk_add_en,
     un_add_in,uz_add_in,uk_add_in,un_add_temp_in,
     tn_add_in,tz_add_in,m_add_in;
     //utilities
-    wire[WORD_SIZE-1:0] adder_result,mul_result,div_result,adder_A_op,adder_B_op;
+    wire[WORD_SIZE-1:0] adder_result_prefinal,adder_result,mul_result,div_result,adder_A_op,adder_B_op,adder_B_op_final,adder_A_op_final;
     wire overflow_flag_mul,overflow_flag_div,overflow_flag_adder,carry_out_adder,negative_adder,
-    mul_done,div_done;
+    mul_done,div_done,deal_as_int;
     wire m_is_zero;
 
     assign m_is_zero = ~& m_value_out;
@@ -63,18 +64,18 @@ wire un_add_en,uz_add_en,uk_add_en,
 
     //Constants
     localparam [ADDRESS_WIDTH-1:0] t0_add = 16'd1,t1_add = 16'd2, m_const_add = 16'd0, u0_add = 16'd10, 
-                    u_add_offset = 16'd4;
+                    u_add_offset = 16'b0000001000000000;
     //Registers
     Register #(.WORD_SIZE(ADDRESS_WIDTH)) un_add      (clk, rst, un_add_en,un_add_in, un_add_out);
     Register #(.WORD_SIZE(ADDRESS_WIDTH)) uz_add      (clk, rst, uz_add_en,adder_result, uz_add_out);
     Register #(.WORD_SIZE(ADDRESS_WIDTH)) uk_add     (clk, rst, uk_add_en, uk_add_in, uk_add_out);
-    Register #(.WORD_SIZE(WORD_SIZE)) un_value      (clk, rst, un_value_en, MDR2_out, un_value_out);
+    Register #(.WORD_SIZE(WORD_SIZE)) un_value      (clk, rst, un_value_en, ram_data2, un_value_out);
     
     Register #(.WORD_SIZE(ADDRESS_WIDTH)) tn_add      (clk, rst, tn_add_en, tn_add_in, tn_add_out);
     Register #(.WORD_SIZE(ADDRESS_WIDTH)) tz_add      (clk, rst, tz_add_en, tz_add_in, tz_add_out);
     Register #(.WORD_SIZE(WORD_SIZE)) tn_value     (clk, rst, tn_value_en,tz_value_out, tn_value_out);
     Register #(.WORD_SIZE(WORD_SIZE)) tk_value      (clk, rst, tk_value_en,tk_port, tk_value_out);
-    Register #(.WORD_SIZE(WORD_SIZE)) tz_value    (clk, rst, tz_value_en,MDR1_out, tz_value_out);
+    Register #(.WORD_SIZE(WORD_SIZE)) tz_value    (clk, rst, tz_value_en,ram_data1, tz_value_out);
     
     Register #(.WORD_SIZE(WORD_SIZE)) temp1      (clk, rst, temp1_en, adder_result, temp1_out);
     Register #(.WORD_SIZE(WORD_SIZE)) temp2      (clk, rst, temp2_en, adder_result, temp2_out);
@@ -82,9 +83,9 @@ wire un_add_en,uz_add_en,uk_add_en,
     
     Register #(.WORD_SIZE(ADDRESS_WIDTH)) mar1      (clk, rst, MAR1_en, MAR1_in, MAR1_out);
     Register #(.WORD_SIZE(ADDRESS_WIDTH)) mar2      (clk, rst, MAR2_en, MAR2_in, MAR2_out);
-    assign MDR2_in = ram_data1;
-    Register #(.WORD_SIZE(WORD_SIZE)) mdr1      (clk, rst, 1'b1, ram_data1, MDR1_out);
-    Register #(.WORD_SIZE(WORD_SIZE)) mdr2      (clk, rst, 1'b1, MDR2_in, MDR2_out);
+    
+    
+    
     
     Register #(.WORD_SIZE(ADDRESS_WIDTH)) m_add      (clk, rst, m_add_en, m_const_add, m_add_out);
     Register #(.WORD_SIZE(WORD_SIZE)) m_value      (clk, rst, m_value_en, m_value_in, m_value_out);
@@ -104,7 +105,7 @@ wire un_add_en,uz_add_en,uk_add_en,
     mux_2_1 #(.SIZE(ADDRESS_WIDTH)) MAR2_mux      (mar2_mux_sel,un_add_temp_out,uk_add_out,MAR2_in);
     mux_2_1 #(.SIZE(WORD_SIZE)) MDR2_mux      (mdr2_mux_sel,ram_data2,temp2_out,MDR2_in);
 
-    mux_2_1 #(.SIZE(WORD_SIZE)) m_value_mux      (m_value_mux_sel,MDR1_out,adder_result,m_value_in);
+    mux_2_1 #(.SIZE(WORD_SIZE)) m_value_mux      (m_value_mux_sel,ram_data1,adder_result,m_value_in);
 
 
     multiplier_booth multiplier (clk,rst,temp1_out,k_out,start_mul,mul_result,overflow_flag_mul,mul_done);
@@ -112,20 +113,25 @@ wire un_add_en,uz_add_en,uk_add_en,
 
     mux_16_1 #(.SIZE(WORD_SIZE)) adder_A_op_mux (adder_opA_mux_sel,
                                                 un_add_out,tz_add_out,uz_add_out,tk_value_out,
-                                                tz_value_out,MDR1_out,MAR1_out,un_add_temp_out,
+                                                tz_value_out,ram_data1,MAR1_out,un_add_temp_out,
                                                 m_value_out,uk_add_out,mul_result,16'd0,
                                                 16'd0,16'd0,16'd0,16'd0,adder_A_op);
 
     mux_8_1 #(.SIZE(WORD_SIZE)) adder_B_op_mux (adder_opB_mux_sel,
-                                                16'd1,tn_value_out,tk_value_out,
-                                                MDR2_out,un_value_out,u_add_offset,
+                                                16'b0000000010000000,tn_value_out,tk_value_out,
+                                                ram_data2,un_value_out,u_add_offset,
                                                 16'd0,16'd0,adder_B_op);
 
+    assign adder_B_op_final = (sub_sg == 1'b1)? ~adder_B_op : adder_B_op;
 
-    carry_select_adder #(.N(WORD_SIZE)) adder (adder_A_op,adder_B_op,1'b0,adder_result,carry_out_adder,
+    assign deal_as_int = (adder_opA_mux_sel == 4'b0000 | adder_opA_mux_sel == 4'b0001 | 
+                                adder_opA_mux_sel == 4'b0010 | adder_opA_mux_sel == 4'b0110 | 
+                                adder_opA_mux_sel == 4'b0111 | adder_opA_mux_sel == 4'b1001 ) ? 1'b1 : 1'b0;
+    assign adder_A_op_final = (deal_as_int == 1'b1) ? {adder_A_op<<7} : adder_A_op;
+    carry_select_adder #(.N(WORD_SIZE)) adder (adder_A_op_final,adder_B_op_final,sub_sg,adder_result_prefinal,carry_out_adder,
                                                 overflow_flag_adder,negative_adder);
 
-
+    assign adder_result = (deal_as_int == 1'b1) ? {adder_result_prefinal>>7}: adder_result_prefinal;
 
     InterpolationFSM fsm (clk,rst,init_sg,alert_sg,update_sg,start_sg,mul_done,div_done,m_is_zero,
                            un_add_en,uz_add_en,uk_add_en,
@@ -146,5 +152,8 @@ wire un_add_en,uz_add_en,uk_add_en,
     
 
     assign mem_write = write_sg;
+    assign data_to_ram = temp2_out;
+    assign ram_add1 = MAR1_out;
+    assign ram_add2 = MAR2_out;
 
 endmodule //
--- Interpolation Module/module.do
+++ Interpolation Module/module.do
@@ -22,3 +22,162 @@ force -freeze sim:/InterpolationModule/ram_data1 0000000000000001 0
 run
 run
 #till here for testing init
+force -freeze sim:/InterpolationModule/tk_port 0000000000000000 0
+force -freeze sim:/InterpolationModule/uk_port 0000000000011110 0
+force -freeze sim:/InterpolationModule/alert_sg 1 0
+run
+force -freeze sim:/InterpolationModule/alert_sg 0 0
+run
+force -freeze sim:/InterpolationModule/tk_port 0000000000000010 0
+force -freeze sim:/InterpolationModule/alert_sg 1 0
+run
+force -freeze sim:/InterpolationModule/alert_sg 0 0
+run
+force -freeze sim:/InterpolationModule/update_sg 1 0
+force -freeze sim:/InterpolationModule/ram_data1 0000000000000011 0
+run
+run
+run
+force -freeze sim:/InterpolationModule/update_sg 0 0
+run
+run
+run
+run
+run
+force -freeze sim:/InterpolationModule/start_sg 1 0
+run
+force -freeze sim:/InterpolationModule/start_sg 0 0
+force -freeze sim:/InterpolationModule/ram_data1 0000000000000001 0
+run
+#now at Start2
+
+#################################
+
+vsim -gui work.InterpolationModule
+add wave -position insertpoint sim:/InterpolationModule/*
+force -freeze sim:/InterpolationModule/start_sg 0 0
+force -freeze sim:/InterpolationModule/alert_sg 0 0
+force -freeze sim:/InterpolationModule/update_sg 0 0
+force -freeze sim:/InterpolationModule/init_sg 1 0
+force -freeze sim:/InterpolationModule/clk 1 0, 0 {50 ps} -r 100
+force -freeze sim:/InterpolationModule/rst 1 0
+force -freeze sim:/InterpolationModule/alert_sg 0 0
+force -freeze sim:/InterpolationModule/init_sg 0 0
+force -freeze sim:/InterpolationModule/tk_port 0000000000000000 0
+force -freeze sim:/InterpolationModule/ram_data1 0000000000000000 0
+force -freeze sim:/InterpolationModule/ram_data2 0000000000000000 0
+run
+force -freeze sim:/InterpolationModule/rst 0 0
+force -freeze sim:/InterpolationModule/init_sg 1 0
+run
+force -freeze sim:/InterpolationModule/init_sg 0 0
+run
+run
+force -freeze sim:/InterpolationModule/ram_data1 0000000000000001 0
+run
+run
+#till here for testing init
+force -freeze sim:/InterpolationModule/tk_port 0000000000000000 0
+force -freeze sim:/InterpolationModule/uk_port 0000000000011110 0
+force -freeze sim:/InterpolationModule/alert_sg 1 0
+run
+force -freeze sim:/InterpolationModule/alert_sg 0 0
+run
+force -freeze sim:/InterpolationModule/tk_port 0000000000000010 0
+force -freeze sim:/InterpolationModule/alert_sg 1 0
+run
+force -freeze sim:/InterpolationModule/alert_sg 0 0
+run
+force -freeze sim:/InterpolationModule/update_sg 1 0
+force -freeze sim:/InterpolationModule/ram_data1 0000000000000011 0
+run
+run
+run
+force -freeze sim:/InterpolationModule/update_sg 0 0
+run
+run
+run
+run
+run
+force -freeze sim:/InterpolationModule/start_sg 1 0
+run
+force -freeze sim:/InterpolationModule/start_sg 0 0
+force -freeze sim:/InterpolationModule/ram_data1 0000000010000000 0
+run
+#now at Start2
+run
+run
+run
+run
+run
+run
+run
+run
+run
+run
+run
+run
+run
+run
+run
+run
+run
+run
+force -freeze sim:/InterpolationModule/ram_data2 0000000000000010 0
+run
+run
+run
+run
+run
+run
+run
+run
+run
+run
+force -freeze sim:/InterpolationModule/m_is_zero 0 0
+run
+run
+run
+run
+run
+run
+run
+run
+run
+run
+run
+run
+run
+run
+run
+run
+run
+run
+run
+run
+run
+run
+run
+run
+run
+run
+run
+run
+run
+run
+force -freeze sim:/InterpolationModule/m_is_zero 1 0
+run
+run
+run
+run
+run
+run
+run
+run
+run
+run
+run
+run
+run
+run
+run
