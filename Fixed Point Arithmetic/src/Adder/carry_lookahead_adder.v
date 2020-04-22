cd90ec25b76fd320bf36f73f0c9558efacd7536d
Ayman <ayman.elakwah@gmail.com>
2020-04-22 11:45:07 +0200

    rename carry_lookahead_adder_behavioral to carry_lookahead_adder

--- Fixed Point Arithmetic/src/Adder/carry_lookahead_adder.v
+++ Fixed Point Arithmetic/src/Adder/carry_lookahead_adder.v
@@ -1,38 +1,31 @@
 module adder
-    #(parameter N=16)
-    (
-        input [N-1:0] A,
-        input [N-1:0] B,
-        input is_subtract,
-        output [N-1:0] result,
-        output carry,
-        output overflow_flag,
-        output negative
-    );
+       #(parameter N=16)
+       (
+           input [N-1:0] A,
+           input [N-1:0] B,
+           input is_subtract,
+           output [N-1:0] result,
+           output reg carry,
+           output reg overflow_flag,
+           output reg negative
+       );
 
-    wire [N:0] cs; // input carries
-    assign cs[0] = is_subtract;
-    wire [N-1:0] G, P;
-    wire [N-1:0] second_operand;
-    assign second_operand = is_subtract ? ~B:B;
+reg [N:0] cs; // input carries
+wire [N-1:0] G, P;
+wire [N-1:0] second_operand;
+assign second_operand = is_subtract ? ~B:B;
+assign G = A & second_operand;
+assign P = A ^ second_operand;
+assign result = P ^ cs[N - 1:0];
 
-    genvar j;
-    generate
-        for (j = 0; j < N-1; j = j+1) begin
-            assign G[j] = A[j] & second_operand[j];
-            assign P[j] = A[j] ^ second_operand[j];
-            assign cs[j+1] = G[j] | cs[j] & P[j];
-        end
-    endgenerate
-
-    full_adder full_adder_0(.i_bit1(A[0]), .i_bit2(second_operand[0]), .i_carry(cs[0]), .o_sum(result[0]));
-    genvar i;
-    generate
-        for (i = 1; i < N - 1; i = i+1) begin
-            full_adder full_adder_x(.i_bit1(A[i]), .i_bit2(second_operand[i]), .i_carry(cs[i]), .o_sum(result[i]));
-        end
-    endgenerate
-    full_adder full_adder_x(.i_bit1(A[N - 1]), .i_bit2(second_operand[N - 1]), .i_carry(cs[N - 1]), .o_sum(result[N - 1]), .o_carry(carry));
-    assign overflow_flag = carry ^ cs[N-1];
-    assign negative = overflow_flag ^ result[N-1];
+always @(*) begin : carry_logic
+    integer i;
+    cs[0] <= is_subtract;
+    for (i = 0; i < N - 1; i = i + 1) begin
+        cs[i+1] <= G[i] | cs[i] & P[i];
+    end
+    carry <= G[N - 1] | cs[N - 1] & P[N - 1];
+    overflow_flag <= carry ^ cs[N-1];
+    negative <= overflow_flag ^ result[N-1];
+end
 endmodule
