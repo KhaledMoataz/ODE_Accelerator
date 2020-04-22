172598b4753ca63ebf88aa061a2d70ef7435a7f5
Yosry Ahmed <yosrym93@gmail.com>
2020-04-22 05:06:12 +0200

    Modifications to step module for synthesizability

--- Step Module/src/StepControlFSM.v
+++ Step Module/src/StepControlFSM.v
@@ -1,7 +1,7 @@
 module StepControlFSM 
 (
 input clk, rst, init, start, multiplier_done, divider_done, adder_overflow, multiplier_overflow, 
-	  divider_overflow,adder_negative_flag, counter_zero,
+	  divider_overflow, adder_negative_flag, counter_zero,
 output error_load, n_load, tolerance_load, memory_read, step_load, adder_is_add, error_clear, 
 		done, proceed, multiplier_start, divider_start, address_load, loop_counter_load, 
 		decrement_counter, increment_addresses, result_load, 
@@ -11,7 +11,7 @@ output [1:0] adder_inputs_selector, multiplier_inputs_selector, address_inputs_s
 );
 	reg [4:0] current_state;
 	reg is_negative;
-	wire calculation_error;
+	reg calculation_error;
 
 	//States
 	localparam IDLE = 5'd0, READ_N_TOLERANCE = 5'd1, READ_STEP = 5'd2, 
@@ -22,17 +22,6 @@ output [1:0] adder_inputs_selector, multiplier_inputs_selector, address_inputs_s
 			   SUB_X  = 5'd11, ACCUMULATE_ERROR = 5'd12, IS_TOLERABLE = 5'd13, DONE_PROCEED = 5'd14,
 			   INIT_CALC_NEW_STEP = 5'd15, WAIT_FOR_CALC_NEW_STEP = 5'd16,
 			   DONE_NO_PROCEED = 5'd17, ERROR = 5'd18;
-
-	assign calculation_error = (adder_overflow & 
-							   (current_state == SUB_X | 
-							    current_state == ACCUMULATE_ERROR | 
-								current_state == IS_TOLERABLE)) |
-							   (multiplier_overflow &
-							   (current_state == WAIT_FOR_CALC_DIV1 |
-							    current_state == WAIT_FOR_CALC_DIV2 |
-								current_state == WAIT_FOR_CALC_DIV3)) |
-							   (divider_overflow &
-							    (current_state == WAIT_FOR_CALC_NEW_STEP));
 	
 	// Control signals
 	assign error_load = current_state == ACCUMULATE_ERROR;
@@ -75,106 +64,117 @@ output [1:0] adder_inputs_selector, multiplier_inputs_selector, address_inputs_s
 	// State transitions
 	always @(posedge clk) 
 	begin
-		is_negative <= adder_negative_flag;
+		is_negative = adder_negative_flag;
+
+		calculation_error = (adder_overflow & 
+							   (current_state == SUB_X | 
+							    current_state == ACCUMULATE_ERROR | 
+								current_state == IS_TOLERABLE)) |
+							   (multiplier_overflow & multiplier_done &
+							   (current_state == WAIT_FOR_CALC_DIV1 |
+							    current_state == WAIT_FOR_CALC_DIV2 |
+								current_state == WAIT_FOR_CALC_DIV3)) |
+							   (divider_overflow & divider_done &
+							    (current_state == WAIT_FOR_CALC_NEW_STEP));
 		if(rst)
-			current_state <= IDLE;
+			current_state = IDLE;
 		else if(calculation_error)
-			current_state <= ERROR;
+			current_state = ERROR;
 		else
 		begin
 			case(current_state)
 				IDLE: 
 				begin
 					if(init)
-						current_state <= READ_N_TOLERANCE;
+						current_state = READ_N_TOLERANCE;
 				end
 				READ_N_TOLERANCE:
 				begin
-					current_state <= READ_STEP;
+					current_state = READ_STEP;
 				end
 				READ_STEP:
 				begin
-					current_state <= INIT_CALC_DIV1;
+					current_state = INIT_CALC_DIV1;
 				end
 				INIT_CALC_DIV1:
 				begin
-					current_state <= WAIT_FOR_CALC_DIV1;
+					current_state = WAIT_FOR_CALC_DIV1;
 				end
 				WAIT_FOR_CALC_DIV1:
 				begin
 					if(multiplier_done)
-						current_state <= INIT_CALC_DIV2;
+						current_state = INIT_CALC_DIV2;
 				end
 				INIT_CALC_DIV2:
 				begin
-					current_state <= WAIT_FOR_CALC_DIV2;
+					current_state = WAIT_FOR_CALC_DIV2;
 				end
 				WAIT_FOR_CALC_DIV2:
 				begin
 					if(multiplier_done)
-						current_state <= INIT_CALC_DIV3;
+						current_state = INIT_CALC_DIV3;
 				end
 				INIT_CALC_DIV3:
 				begin
-					current_state <= WAIT_FOR_CALC_DIV3;
+					current_state = WAIT_FOR_CALC_DIV3;
 				end
 				WAIT_FOR_CALC_DIV3:
 				begin
 					if(multiplier_done)
-						current_state <= DONE_INIT;
+						current_state = DONE_INIT;
 				end
 				DONE_INIT:
 				begin
 					if(start)
-						current_state <= INIT_ERROR_CALC;
+						current_state = INIT_ERROR_CALC;
 				end
 				INIT_ERROR_CALC:
 				begin
-					current_state <= SUB_X; 
+					current_state = SUB_X; 
 				end
 				SUB_X: 
 				begin
-					current_state <= ACCUMULATE_ERROR;
+					current_state = ACCUMULATE_ERROR;
 				end
 				ACCUMULATE_ERROR:
 				begin
 					if(counter_zero)
-						current_state <= IS_TOLERABLE;
+						current_state = IS_TOLERABLE;
 					else
-						current_state <= SUB_X; 
+						current_state = SUB_X; 
 				end
 				IS_TOLERABLE:
 				begin
 					if(adder_negative_flag)
-						current_state <= DONE_PROCEED;
+						current_state = DONE_PROCEED;
 					else
-						current_state <= INIT_CALC_NEW_STEP;
+						current_state = INIT_CALC_NEW_STEP;
 				end
 				DONE_PROCEED:
 				begin
 					if(start)
-						current_state <= INIT_ERROR_CALC;
+						current_state = INIT_ERROR_CALC;
 				end
 				INIT_CALC_NEW_STEP:
 				begin
-					current_state <= WAIT_FOR_CALC_NEW_STEP;
+					current_state = WAIT_FOR_CALC_NEW_STEP;
 				end
 				WAIT_FOR_CALC_NEW_STEP:
 				begin
 					if(divider_done)
-						current_state <= DONE_NO_PROCEED;
+						current_state = DONE_NO_PROCEED;
 				end
 				DONE_NO_PROCEED:
 				begin
 					if(start)
-						current_state <= INIT_ERROR_CALC;
+						current_state = INIT_ERROR_CALC;
 				end
 				ERROR:
 				begin
 					if(init)
-						current_state <= READ_N_TOLERANCE;
+						current_state = READ_N_TOLERANCE;
 					else if(start)
-						current_state <= INIT_ERROR_CALC;
+						current_state = INIT_ERROR_CALC;
 				end
 			endcase
 		end
