cb0a39e572f344bfc943686b469e8873d76b479a
philopateer nabil <41947911+phella@users.noreply.github.com>
2020-04-25 18:18:33 +0200

    fix decompressor test

--- IO module/decompressor.test.tcl
+++ IO module/decompressor.test.tcl
@@ -1,66 +1,95 @@
-vsim -gui work.decomp2
-add wave -position insertpoint sim:/decomp2/*
+vsim -gui work.decompressor
+add wave -position insertpoint sim:/decompressor/*
 
 
 
-force -freeze sim:/decomp2/clk 0 0
-force -freeze sim:/decomp2/reset 1 0
+force -freeze sim:/decompressor/clk 0 0
+force -freeze sim:/decompressor/reset 1 0
 run
 
-force -freeze sim:/decomp2/clk 1 0
-force -freeze sim:/decomp2/value 000100 0
-force -freeze sim:/decomp2/bit 1 0
-force -freeze sim:/decomp2/start 1 0
-force -freeze sim:/decomp2/reset 0 0
+force -freeze sim:/decompressor/clk 1 0
+force -freeze sim:/decompressor/value 000100 0
+force -freeze sim:/decompressor/bit 1 0
+force -freeze sim:/decompressor/start 1 0
+force -freeze sim:/decompressor/reset 0 0
 
-set work [ examine sim:/decomp2/work]
+set work [ examine sim:/decompressor/work]
 
 if { $work == 0 } {
-	echo "decomp2 should start working"
+	echo "decompressor should start working"
 }
 
 run
-
-force -freeze sim:/decomp2/clk 0 0
-force -freeze sim:/decomp2/start 0 0
+force -freeze sim:/decompressor/clk 0 0
+run
+force -freeze sim:/decompressor/start 0 0
+force -freeze sim:/decompressor/clk 1 0
 run
 
-force -freeze sim:/decomp2/value 001000 0
-force -freeze sim:/decomp2/bit 0 0
-force -freeze sim:/decomp2/start 1 0
-force -freeze sim:/decomp2/clk 1 0
 
-set work [ examine sim:/decomp2/work]
+
+force -freeze sim:/decompressor/clk 0 0
+run
+
+
+
+force -freeze sim:/decompressor/value 001000 0
+force -freeze sim:/decompressor/bit 0 0
+force -freeze sim:/decompressor/clk 1 0
+
+set work [ examine sim:/decompressor/work]
 
 if { $work == 0 } {
-	echo "decomp2 should start working"
+	echo "decompressor should start working"
 }
 
-run
-
-force -freeze sim:/decomp2/clk 0 0
-run
-
-force -freeze sim:/decomp2/value 010100 0
-force -freeze sim:/decomp2/bit 1 0
-force -freeze sim:/decomp2/start 1 0
-force -freeze sim:/decomp2/clk 1 0
 
 run
+force -freeze sim:/decompressor/start 1 0
+force -freeze sim:/decompressor/clk 0 0
+run
+force -freeze sim:/decompressor/clk 1 0
+run
 
-set data [ examine -binary sim:/decomp2/out]
+force -freeze sim:/decompressor/start 0 0
+force -freeze sim:/decompressor/clk 0 0
+run
+
+
+force -freeze sim:/decompressor/clk 1 0
+run
+
+
+force -freeze sim:/decompressor/clk 0 0
+run
+
+force -freeze sim:/decompressor/clk 1 0
+run
+
+force -freeze sim:/decompressor/value 010100 0
+force -freeze sim:/decompressor/bit 1 0
+force -freeze sim:/decompressor/clk 1 0
+
+run
+
+
+force -freeze sim:/decompressor/start 1 0
+force -freeze sim:/decompressor/clk 0 0
+run
+
+
+force -freeze sim:/decompressor/clk 1 0
+run
+
+
+set data [ examine -binary sim:/decompressor/out]
 
 if { $data != "11111111111111111111000000001111" } {
 	echo "decompressor wrongly decompressed"
 }
 
 
-force -freeze sim:/decomp2/start 0 0
-force -freeze sim:/decomp2/clk 0 0
-run
 
-force -freeze sim:/decomp2/clk 1 0
-run
-
-force -freeze sim:/decomp2/clk 0 0
+force -freeze sim:/decompressor/start 0 0
+force -freeze sim:/decompressor/clk 0 0
 run
